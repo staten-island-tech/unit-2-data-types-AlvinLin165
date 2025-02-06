6c6f7572c89f0efe941b90950acaeae4ae1fb32e
AlvinLin165 <alvinlin165@gmail.com>
2025-02-06 10:17:42 -0500

    cool mad libs yes i add many things pls fporhgihe me

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-x = 3
+""" x = 3
 y = float(3)
 print(x,y)
 
-values = [1,2.23,5,7,2,30,15]
+values = [1,2.23,5,7,2,30,1, 5]
 print(values)
 for i in values:
     print(i)
 
 print(values[0])
 print(values[7])
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+#this is chat gpt btw
+sentence = input("write a sentence plspls pls do it for me")
+word = len(sentence.split()) #len counts individual letters, use split to count words
+print("ill call you", word, "times a day")
+
+
+sentence = input("enter noun, noun, adjective, verb, verb, noun, adjective, gloogorb, and your name")
+
+nouna = 
+nounb =
+adjective = 
+verbs = 
+verba =
+nounc = 
+adjectivea = 
+gloogorb = 
+name = 
